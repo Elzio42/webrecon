5725c01e96388e7c47140046aa10de3e828cdd0f
Elzio42 <claudiojuniorp18@gmail.com>
2020-03-23 00:40:05 -0400

    Incluída busca por arquivos através de parâmetro

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # webrecon
+
+Em desenvolvimento...
+
+Modo de uso: $ /webrecon.sh <dominio> <tipo de arquivo> 
+
+=> Sem parâmetros especificados, irá efetuar uma busca por diretórios e os tipos de arquivos principais
+=> Um parâmetro poderá ser especificado para busca somente de diretórios ou somente de arquivos
+=> Um parâmetro poderá ser repassado para filtrar a busca por determinados tipos de arquivos
+=> Uma lista pode ser passada como parâmetro
+
+Atualmente:
+
+=> O dominio e o tipo de arquivo é passado como parâmetro.
--- webrecon.sh
+++ webrecon.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
 
-for palavra in $(cat lista.txt)
-do
-resposta=$(curl -s -o /dev/null -w "%{http_code}" http://$1/$palavra/)
-if [ $resposta == "200" ]
+declare -a diretorios
+declare -a arquivos
+
+nc='\033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+
+
+exibir_resultado () {
+if [ "$2" == "arquivos" ]
 then
-echo "Diretório encontrado: http://$1/$palavra/"
+  declare -n var=$2 # '-n' faz da variável um ponteiro para outra variável.
+  printf "${blue}\nArquivos encontrados${nc}\n"
+else
+  declare -n var=$2 
+  printf "${blue}\nDiretórios encontrados${nc}\n"
+fi
+
+for i in "${var[@]}"
+do
+  echo "http://$1/${i}"
+done
+}
+
+
+for palavra in $(cat listateste.txt)
+do
+diretorio=$(curl -s -H "User-Agent: elzio" -o /dev/null -w "%{http_code}" http://$1/$palavra/)
+arquivo=$(curl -s -H "User-Agent: elzio" -o /dev/null -w "%{http_code}" http://$1/$palavra.$2)
+
+if [ $diretorio == "200" ]
+then
+  diretorios+=($palavra)
+fi
+
+if [ $arquivo == "200" ]
+then
+  arquivos+=($palavra.$2)
 fi
 done
 
+if [ ! -z "$diretorios" ]
+then
+  exibir_resultado $1 diretorios
+fi
 
+if [ ! -z "$arquivos" ]
+then
+  exibir_resultado $1 arquivos
+fi
